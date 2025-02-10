35f14dbdae724bb87ba3d2fa851457f3c63975d3
balaji <balajier2006@gmail.com>
2025-02-10 14:39:13 +0530

    done

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.post('/signup',(req,res)=>{
         return res.status(400).Json({message:'Username cannot be empty'});
     if(!email)
         return res.status(400).json({message:'Email cannot be empty'});
-    if(password.length >8 || password.length <=16)
+    if(password.length < 8 || password.length > 16)
         return res.status(400).json({message:'Password length should be greater than 8 or less than or equal to 16'});
     return res.status(200).json({message:`server is succesfully running`})
 })
